3c43eb54deb899269b45db478f77dd5dbd03166e
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-03-12 17:31:14 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,3 +1,15 @@
 #installing new packages in R
+
 install.packages("Terra")
 library(terra)
+
+install.packages("devtools")
+library(devtools)
+
+installing packaging from Github:
+devtools::install_Github #"::" significa che install_Github è una funzione in devtools
+
+install the imageRy package from GitHub
+#a devtools function
+devtools::install_github("ducciorocchini/imageRy")
+library(imageRy)

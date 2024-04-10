21a49c41b30fa6272c85aec4459fd898343ce965
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-04-10 12:08:29 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -9,11 +9,11 @@ library(ggplot2)
 #listing images
 im.list()
 
-#importaing data
+#importaing matogrosso images
 m1992<-im.import("matogrosso_l5_1992219_lrg.jpg")
-> m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
+m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
 
-#classifying sun image
+#importing and classifying sun image
 sun<-im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 > sunc<-im.classify(sun,num_cluster=3)
 
@@ -21,9 +21,23 @@ sun<-im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 > m1992c<-im.classify(m1992,num_cluster=2)
 > m2006c<-im.classify(m2006,num_cluster=2)
 
-# calculating frequences --> n of pixel
+# calculating ferquences 1992 image
 f1992<-freq(m1992c)
 f1992
+
+#calculating total n of pixel 1992 image
+tot1992<-ncell(m1992)
+tot1992
+
+# calculating prop 1992 image
+> prop1992=f1992/tot1992
+
+# calculating
+> perc1992=prop1992*100
+
+#% = forest 83%, human 16%
+
+# repeating the math for 2006 image
 
 #% = forest 45%, human 55%
 

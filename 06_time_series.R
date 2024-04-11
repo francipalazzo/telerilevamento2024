1177d4ab16cb39da1505763adf9bddc6ad1bb610
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-04-11 17:36:43 +0200

    Create 06_time_series.R

---
+++ 06_time_series.R
@@ -0,0 +1,49 @@
+# other method to see changes in time
+#the first one was by classificating the image
+
+library(terra)
+library(imageRy)
+
+im.list()
+
+#importing data
+en01<-im.import("EN_01.png")
+en13<-im.import("EN_13.png")
+
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+# using the first element (band) of images
+dif = EN01[[1]] - EN13[[1]]
+
+# palette
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)
+plot(dif, col=cldif)
+
+#ice melt in greenland
+
+g2000 <- im.import("greenland.2000.tif")
+clg <- colorRampPalette(c("black", "blue", "white", "red")) (100)
+plot(g2000, col=clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+plot(g2015, col=clg)
+
+par(mfrow=c(1,2))
+plot(g2000, col=clg)
+plot(g2005, col=clg)
+plot(g2010, col=clg)
+plot(g2015, col=clg)
+
+# stacking the data
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
+
+# Exercise: make the differencxe between the first and the final elemnts of the stack
+difg <- stackg[[4]] - stackg[[1]]
+# difg <- g2000 - g2015
+plot(difg, col=cldif)

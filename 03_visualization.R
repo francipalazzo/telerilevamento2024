104045b35ba9790b9334ef97e63a9e66df22505b
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-05-21 13:04:51 +0200

    Update 03_visualization.R

--- 03_visualization.R
+++ 03_visualization.R
@@ -1,16 +1,16 @@
 library(terra) 
 library(imageRy)
 
-#list of data in imageRy im.list
+#list of data in imageRy 
+im.list
 
 #importing data 
 b2<-im.import("sentinel.dolomites.b2.tif")
 
-#plotting data
+#plotting data and choosing a color palette
 plot(b2)
 > clg<-colorRampPalette(c("black","grey","light grey"))(3)
 > plot(b2,col=clg)
-
 viola<-colorRampPalette(c("magenta","cyan4","cyan"))(100)
 > plot(b2,col=viola)
 
@@ -22,34 +22,36 @@ importing new bands
 > b8<-im.import("sentinel.dolomites.b8.tif")
 > plot(b8,col=viola)
 
-#multiframe
+#making a multiframe
 > par(mfrow=c(2,2))
 > plot(b2,col=viola)
 > plot(b3,col=viola)
 > plot(b4,col=viola)
 > plot(b8,col=viola) 
 
-#creating a satelltite image
+#making a stack and creating a satelltite image
 stacksent<-c(b2,b3,b4,b8)
 plot(stacksent,col=viola)
 
->plot(stacksent[[4]],col=viola) #per fare lo stack di un solo elemento
->dev.off() #per cancellare una delle funzioni par precedenti
+>plot(stacksent[[4]],col=viola) #this funcion allows you to make a stack of one element 
+>dev.off() #this funcion allows you to delete the previous stack
 
 #RGB plotting
 #stacksent[(1)] = b2 = blue
 #stacksent[(2)] = b3 = green
 #stacksent[(3)] = b4 = red
-#stacksent[(4)] = b8 = nir (vicino infrarosso)
+#stacksent[(4)] = b8 = nir (nir infrared)
 
+#making a plot
 im.plotRGB(stacksent, 3, 2, 1)
-im.plotRGB(stacksent, 4, 2, 1) # ho sostituito il rosso con l'infrarosso
+im.plotRGB(stacksent, 4, 2, 1) #i switched the red with the infrared
 
 #dev.off()
-#vediamo che il colore non cambia perchè l'infrarosso è il meno correlato alle altre bande
+
 im.plotRGB(stacksent, 3, 2, 1)
 im.plotRGB(stacksent, 4, 2, 1)
 im.plotRGB(stacksent, 4, 3, 2)
+#these plots showed that the colour its the same cause the infrared its not correlated to the other bands much
 
 #nir on green
 im.plotRGB(stacksent, 3, 4, 2)

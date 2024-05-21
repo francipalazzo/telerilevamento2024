a84a481a8b0a875e5905f0080b0415dea9e5168d
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-05-21 15:20:20 +0200

    Update 07_importing_external_data.R

--- 07_importing_external_data.R
+++ 07_importing_external_data.R
@@ -1,9 +1,10 @@
 #importing data from external sources in R
 
+#lounching the packages 
 library(terra)
 library(imageRy)
 
-#setting the right directoty on my PC
+#setting the right directoty on my PC 
 setwd("/Users/franc/Downloads")
 
 #importing data on my PC
@@ -12,7 +13,7 @@ eclissi<-rast("eclissi.png")
 #to see the image
 plot(eclissi)
 
-#now i can use te image however i want
+#now i can use te image however i want in R
 
 #let's import another immage taken fron earth observatory website 
 setwd("/Users/franc/Downloads")
@@ -20,7 +21,8 @@ earth<-rast("earth.jpg")
 plot(earth)
 
 #login and getting images from the copernicus website
-#importing the images in R by using the same steps used before 
+#importing the images in R by using the same steps used before
+
 #cropping data
 ext <- c(25, 35, 58, 62)
 soilcrop <- crop(copernicus, ext)

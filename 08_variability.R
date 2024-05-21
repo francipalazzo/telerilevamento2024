8e2c8f6fbedb543bb0f87658595dff11c151e20f
francipalazzo <162352949+francipalazzo@users.noreply.github.com>
2024-05-21 15:33:57 +0200

    Update 08_variability.R

--- 08_variability.R
+++ 08_variability.R
@@ -2,41 +2,40 @@
 
 library(terra)
 library(imageRy)
-
 im.list()
-im.import("sentinel.png")
 
+#lounching the image and making a plot with the RGB scheme
 sent<-im.import("sentinel.png")
 plotRGB(sent,1,2,3)
+plotRGB(sent,2,1,3)
 
 # NIR = band 1
 # red = band 2
 # green = band 3
 
-#scelta banda  
+#it is necessery to choose a band sobjectively
 nir<-sent[[1]]
 plot(nir)
-col<-colorRampPalette(c("red","orange","yellow"))(100) #scegliamo la palette
+col<-colorRampPalette(c("red","orange","yellow"))(100) #choosing a palette
 plot(nir,col=col)
 
->sd3<-focal(nir, matrix(1/9,3,3), fun=sd) #(con focal faccio il calcolo su una matrice 3x3)
->plot(sd3)
+#calculating the standard variation
+sd3<-focal(nir, matrix(1/9,3,3), fun=sd) #(with "focal" i can calculate a matrix of 3x3)
+plot(sd3)
 
-#installing "viridis" package #serve per fare in modo che anche i daltonici vedano le mappe
+#installing and lounching the "viridis" package, useful because it allows you to create maps that also daltonic people can see
 install.packages("viridis")
 library(viridis)
 
-#cercando su google ci escono delle palette già predefinite con i propri nomi e noi utilizziamo quella chiamata viridis
-
+#we can search on google the names of the various palettes we can use
 viridisc<-colorRampPalette(viridis(7))(100)
 plot(sd3, col=viridisc)
 
-#esercozio --> ripetiamo gli stessi calcoli considerando una finestra di 7 pixel
+#exercise --> let's repeat the math calculating the standard variation of a window of 7 pixels
 sd4<-focal(nir, matrix(1/49,7,7), fun=sd)
 plot(sd4)
 viridisc<-colorRampPalette(viridis(7))(100)
 plot(sd4, col=viridisc)
-
 #facciamo uno stack
 sdstack<-c(sd3,sd4)
 plot(sdstack, col=viridisc) 
